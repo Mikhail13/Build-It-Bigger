1c2d3a8344cef31651a8522dbedcd42dde8fca16
Mikhail Sukharuchkin <sukharuchkinm@gmail.com>
2016-03-07 23:39:58 +0200

    FinalProject with JokesLib

---
+++ jokeslib/src/main/java/za/co/mikhails/nanodegree/jokeslib/JokeSource.java
@@ -0,0 +1,16 @@
+package za.co.mikhails.nanodegree.jokeslib;
+
+import java.util.Random;
+
+public class JokeSource {
+    private Random random = new Random();
+    private String[] jokes = {"Chuck Norris can make a class that is both abstract and final.",
+            "Chuck Norris can retrieve anything from /dev/null.",
+            "Chuck Norris knows the last digit of pi.",
+            "Chuck Norris can compile syntax errors.",
+            "Chuck Norris can unit test entire applications with a single assert."};
+
+    public String getJoke() {
+        return jokes[random.nextInt(5)];
+    }
+}
